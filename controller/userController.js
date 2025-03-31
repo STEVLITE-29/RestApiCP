8c94b8361a0c8cccf6be9d0b7322280737739935
Stephen Oghenefego Okeh <stevlite29@gmail.com>
2025-03-31 17:43:17 +0100

    Crud operstion complete

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,17 @@
 import User from "../models/User.js";
 
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find()
+        if (users.length === 0) {
+            return res.status(404).json({message: "No users found."})
+        }
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({error: "internal Server Error"})
+    }
+}
+
 export const create = async (req, res) => {
     try {
         const userData = new User(req.body);
@@ -13,14 +25,36 @@ export const create = async (req, res) => {
         const savedUser = await userData.save();
         res.status(200).json(savedUser);
     } catch (error) {
-        res.status(500).json({error: "internal Server error"})
+        res.status(500).json({error: "internal Server Error"})
     }
 }
 
-export const helloMsg = async (req, res) => {
+
+export const updateUser = async (req, res) => {
     try {
-        return res.json("Hello World")
+        const id = req.params.id;
+        const userExist = await User.findOne({_id: id});
+        if (!userExist) {
+            res.status(404).json({message: "User not found."});
+        }
+        const update = await User.findByIdAndUpdate(id, req.body, {new: true});
+        res.status(201).json(update);
     } catch (error) {
-        res.status(500).json({error: "internal Server error"})
+        res.status(500).json({error: "internal Server Error"})
     }
 }
+
+export const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userExist = await User.findById(id);
+        if (!userExist) {
+            return res.status(404).json({ message: "User not found." });
+        }
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ message: "User deleted successfully." });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {helloMsg, create} from "../controller/userController.js"
+import {getUsers, create, updateUser, deleteUser} from "../controller/userController.js"
 
 const userRoute = express.Router();
 
 userRoute.post('/create', create);
-userRoute.get("/fetch", helloMsg)
+userRoute.get("/getAllUsers", getUsers)
+userRoute.put("/update/:id", updateUser)
+userRoute.delete("/delete/:id", deleteUser)
 
 export default userRoute; 
